f46583274df79cf0d6353893ade1c4624faee9c3
Tech_Aaru <shahapurkarariyan@gmail.com>
2025-11-19 17:41:58 +0530

    adding taint-practice

--- day-11-multiaccount/main.tf
+++ day-11-multiaccount/main.tf
@@ -3,11 +3,10 @@ resource "aws_instance" "name" {
   ami="ami-07860a2d7eb515d9a" 
   instance_type = "t3.micro"
 
+
 }
 
-#resource "aws_s3_bucket" "name" {
-    #bucket = "tybuwbdsjbcscj-bucket"
-  #  provider = aws.dublin
+
   
 } 
   #instance_type = "t3.micro"
